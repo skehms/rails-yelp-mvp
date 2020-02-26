3c44f2cfaa1e2884940133a3279f07d8b383436a
Sarah Kehm <sarah@k-e-h-m.de>
2020-02-26 14:36:57 +0800

    Solved rake

---
+++ app/models/review.rb
@@ -0,0 +1,6 @@
+class Review < ApplicationRecord
+  belongs_to :restaurant
+  validates :restaurant_id, presence: true
+  validates :content, presence: true
+  validates :rating, presence: true, numericality: true, inclusion: { in: [0, 1, 2, 3, 4, 5] }
+end
